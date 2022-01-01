6ccfdcf8327463eb9963cd0e2e77f3b85361ab77
ProgramerArt123 <2039231428@qq.com>
2022-01-01 09:46:23 +0800

    collation config

--- c_front.s
+++ c_front.s
@@ -9,15 +9,19 @@ string:\"[("\".)^\"]*\"
 
 integer:[0-9]+
 
-label:[_a-zA-Z][_a-zA-Z0-9]*
+label_head<:[_a-zA-Z]
 
-colon:":"
+label:@label_head@[_a-zA-Z0-9]*
+
+
 
 semicolon:";"
 
-oper_comma:","
+comma:","
 
-oper_as:"="
+colon:":"
+
+oper_eq:"="
 
 oper_inc:"++"
 
@@ -37,25 +41,7 @@ block_left:"{"
 
 block_right:"}"
 
-round:@round_left@@expression@@round_right@
-
-variable_def:(@arbitrarily_type@@segmentation@@variable@(@oper_as@@expression@)?)#VariableDef#
-
-statement:[@return_s@@goto@@proc_call@@variable_def@@expression@]
-
-expression:[(@oper_one@@variable@)(@variable@[@oper_inc@@oper_dec@])(@element@(@oper_two@@element@)*)]
-
-element:[@round@@proc_call@@string@@variable@@numeric@@integer@@address_s@@sizeof_s@]
-
-sizeof:"sizeof"
-
-sizeof_s:[(@sizeof@@round@)(@sizeof@@element@)]
-
-oper_two:[@oper_comma@@oper_assgin@@oper_logic_or@
-@oper_logic_and@@oper_bit_or@@oper_bit_xor@@oper_bit_and@
-@oper_equal@@oper_rela@@oper_mov@@oper_add_sub@@oper_mul_div@]
-
-oper_assgin:[@oper_as@"+=""-=""*=""/=""%=""&=""^=""|=""<<="">>="]
+oper_assgin:[@oper_eq@"+=""-=""*=""/=""%=""&=""^=""|=""<<="">>="]
 
 oper_logic_or:"||"
 
@@ -79,13 +65,14 @@ oper_mul_div:["*""/""%"]
 
 oper_one:["-"@oper_inc@@oper_dec@"!""~""+""-""*""&"]
 
-type_cast:@round_left@@label@@round_right@
-
 oper_suffix:[@oper_dot@"->"]
 
-numeric:@integer@(@oper_dot@@integer@)?
+oper_two:[@comma@@oper_assgin@@oper_logic_or@
+@oper_logic_and@@oper_bit_or@@oper_bit_xor@@oper_bit_and@
+@oper_equal@@oper_rela@@oper_mov@@oper_add_sub@@oper_mul_div@]
 
-variable:@pointer@*@label@(@square_left@@expression@@square_right@)*(@oper_suffix@@variable@)*
+
+
 
 int<:["int""long""short""char"]
 
@@ -97,72 +84,141 @@ void<:"void"
 
 sign<:["unsigned""signed"]
 
+const<:"const"
+
+extern_static<:["extern""static"]
+
+register_volatile<:["register""volatile"]
+
+return<:"return"
+
+enum<:"enum"
+
+struct<:"struct"
+
+union<:"union"
+
+sizeof<:"sizeof"
+
 base_type<:[@int@@double@@float@@void@(@sign@(@segmentation@@int@)?)]
 
 type<:[@base_type@@label@]
 
-const<:"const"
-
 const_type<:[(@const@@segmentation@@type@)(@type@@segmentation@@const@)]
 
-extern_static<:["extern""static"]
+extern_static_type<:[(@extern_static@@segmentation@@type@)
+	(@type@@segmentation@@extern_static@)]
 
-extern_static_type<:[(@extern_static@@segmentation@@type@)|(@type@@segmentation@@extern_static@)]
+register_volatile_type<:[(@register_volatile@@segmentation@@type@)
+	(@type@@segmentation@@register_volatile@)]
 
-register_volatile<:["register""volatile"]
+extern_static_const<:[(@extern_static@@segmentation@@const@)
+	(@const@@segmentation@@extern_static@)]
 
-register_volatile_type<:[(@register_volatile@@segmentation@@type@)(@type@@segmentation@@register_volatile@)]
+extern_static_const_type<:[(@extern_static_const@@segmentation@@type@)
+	(@type@@segmentation@@extern_static_const@)]
 
-extern_static_const<:[(@extern_static@@segmentation@@const@)(@const@@segmentation@@extern_static@)]
+extern_static_register_volatile<:[(@extern_static@@segmentation@@register_volatile@)
+	(@register_volatile@@segmentation@@extern_static@)]
 
-extern_static_const_type<:[(@extern_static_const@@segmentation@@type@)(@type@@segmentation@@extern_static_const@)]
+extern_static_register_volatile_type<:[(@extern_static_register_volatile@@segmentation@@type@)
+	(@type@@segmentation@@extern_static_register_volatile@)]
 
-extern_static_register_volatile<:[(@extern_static@@segmentation@@register_volatile@)(@register_volatile@@segmentation@@extern_static@)]
+const_register_volatile<:[(@const@@segmentation@@register_volatile@)
+	(@register_volatile@@segmentation@@const@)]
 
-extern_static_register_volatile_type<:[(@extern_static_register_volatile@@segmentation@@type@)(@type@@segmentation@@extern_static_register_volatile@)]
-
-const_register_volatile<:[(@const@@segmentation@@register_volatile@)(@register_volatile@@segmentation@@const@)]
-
-const_register_volatile_type<:[(@const_register_volatile@@segmentation@@type@)(@type@@segmentation@@const_register_volatile@)]
+const_register_volatile_type<:[(@const_register_volatile@@segmentation@@type@)
+	(@type@@segmentation@@const_register_volatile@)]
 
 arbitrarily_type<:[@const_register_volatile_type@@extern_static_register_volatile_type@
-@extern_static_const_type@@register_volatile_type@@extern_static_type@@const_type@@type@]
+	@extern_static_const_type@@register_volatile_type@@extern_static_type@@const_type@@type@]
+
+
 
 pointer:"*"
 
-address:"&"
+if:"if"
 
-address_s:@address@@variable@
+else:"else"
+
+break:"break"
+
+case:"case"
+
+default:"default"
+
+continue:"continue"
+
+while:"while"
+
+do:"do"
+
+for:"for"
+
+
+
+
+
+numeric:@integer@(@oper_dot@@integer@)?
+
+variable:@pointer@*@label@(@square_left@@expression@@square_right@)*(@oper_suffix@@variable@)*
+
+sizeof_s:@sizeof@[(@segmentation@?@round@)(@segmentation@@element@)]
+
+element:[@round@@proc_call@@string@@variable@@numeric@@integer@@address@@sizeof_s@]
+
+address:@oper_bit_and@@variable@
+
+type_cast:@round_left@@label@@round_right@
+
+expression:[(@oper_one@@variable@)
+			(@variable@[@oper_inc@@oper_dec@])
+			(@element@(@oper_two@@element@)*)]
+
+return_s:@return@(@segmentation@@expression@)?
+
+goto:goto@label@
+
+proc_call:@variable@@round_left@@arguments@?@round_right@
+
+
+
+
+
+
+round:@round_left@@expression@@round_right@
+
+statement:[@return_s@@goto@@proc_call@@variable_def@@expression@]
 
 block:@block_left@(@statement@@semicolon@)*@block_right@
 
 scope:[@block@(@statement@)]
 
-if:if@round@@scope@
+comma_expression:@expression@(@comma@@expression@)*
 
-else_if:elseif@round@@scope@
+arguments:@expression@(@comma@@expression@)*
 
-else:else@round@@scope@
+parameters:	@arbitrarily_type@@label@(@arbitrarily_type@@label@)*
 
-if_else:@if@(@else_if@)*(@else@)?
+assgin_label:@label@(@oper_eq@@integer@)?
 
-break:"break"
+body:@label@@block_left@([(@variable_def@@semicolon@)
+	@enum_def@@struct_def@@union_def@])*@block_right@@semicolon@
+
+
+
+
+
 
 break_process:[@break@@return_s@@goto@@proc_call@@expression@]
 
 branch:@integer@@colon@[@block@(@break_process@+)]?
 
-case:"case"
-
 case_s:@case@@branch@
-
-default:"default"
 
 default_s:@default@@branch@
 
 switch_case:@round@@block_left@@case_s@*@default_s@?@block_right@
-
-continue:"continue"
 
 break_continue_statement:[@break@@continue@@statement@]
 
@@ -170,47 +226,43 @@ break_continue_block:@block_left@@break_continue_statement@*@block_right@
 
 once:[@break_continue_block@(@break_continue_statement@)]
 
-while:"while"
+
+
+
+
+if_s:@if@@round@@scope@
+
+else_if_s:@else@@if@@round@@scope@
+
+if_else_s:@if_s@(@else_if_s@)*(@else_s@)?
+
+else_s:@else@@round@@scope@
+
+do_while_s:@do@@once@@while@@round@
 
 while_s:@while@@round@@once@
 
-do:"do"
+for_s:@for@@round_left@(@statement@(@comma@@statement@)*)|
+	@semicolon@@comma_expression@|@semicolon@@comma_expression@|@round_right@@once@
 
-do_while:@do@@once@@while@@round@
 
-comma_expression:@expression@(@oper_comma@@expression@)*
 
-for:"for"
 
-for_s:@for@@round_left@(@statement@(@oper_comma@@statement@)*)|@semicolon@@comma_expression@|@semicolon@@comma_expression@|@round_right@@once@
 
-arguments:@expression@(@oper_comma@@expression@)*
 
-proc_call:@variable@@round_left@@arguments@?@round_right@
 
-parameters:	@arbitrarily_type@@label@(@arbitrarily_type@@label@)*
 
-proc_def:(@arbitrarily_type@@segmentation@@label@@round_left@@parameters@?@round_right@@block@)#ProcDef#
 
-return<:"return"
+variable_def:(@arbitrarily_type@@segmentation@@variable@
+	(@oper_eq@@expression@)?)#VariableDef#
 
-return_s:@return@(@segmentation@@expression@)?
+proc_def:(@arbitrarily_type@@segmentation@@label@
+	@round_left@@parameters@?@round_right@@block@)#ProcDef#
 
-goto:goto@label@
-
-assgin_label:@label@(@oper_as@@integer@)?
-
-enum<:"enum"
-
-enum_def:(@enum@@segmentation@@label@@block_left@(@assgin_label@@oper_comma@?)*@block_right@@semicolon@)#EnumDef#
-
-body:@label@@block_left@([(@variable_def@@semicolon@)@enum_def@@struct_def@@union_def@])*@block_right@@semicolon@
-
-struct<:"struct"
+enum_def:(@enum@@segmentation@@label@@block_left@
+	(@assgin_label@@comma@?)*@block_right@@semicolon@)#EnumDef#
 
 struct_def:(@struct@@segmentation@@body@)#StructDef#
-
-union<:"union"
 
 union_def:(@union@@segmentation@@body@)#UnionDef#
 
