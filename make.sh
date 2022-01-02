b13669b0c58a10cbbc75128ae3a426059156b5c0
ProgramerArt123 <2039231428@qq.com>
2022-01-02 11:49:35 +0800

    generate scanner share lib

--- make.sh
+++ make.sh
@@ -1,2 +1,8 @@
+#! /bin/bash
+
+rm -f libScanner.so
+g++ -std=c++11 -fPIC -shared AndPattern.cpp CharPattern.cpp CodeGenerate.cpp Config.cpp Content.cpp Lexical.cpp OrPattern.cpp Pattern.cpp StringPattern.cpp Rule.cpp -o libScanner.so
+
 rm -f Scanner
-g++ -std=c++11 *.cpp -o Scanner
+g++ -std=c++11 Scanner.cpp -L. -lScanner -o Scanner
+
