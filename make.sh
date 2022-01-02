257393b62aef0faf1da2011669ae70171ae2a4d3
ProgramerArt123 <2039231428@qq.com>
2022-01-02 12:33:48 +0800

    generate libc_front.so

--- make.sh
+++ make.sh
@@ -4,5 +4,9 @@ rm -f libScanner.so
 g++ -std=c++11 -fPIC -shared AndPattern.cpp CharPattern.cpp CodeGenerate.cpp Config.cpp Content.cpp Lexical.cpp OrPattern.cpp Pattern.cpp StringPattern.cpp Rule.cpp -o libScanner.so
 
 rm -f Scanner
-g++ -std=c++11 Scanner.cpp -L. -lScanner -o Scanner
+g++ -std=c++11 Scanner.cpp -L. -lScanner -o Scanner -Wl,-rpath .
 
+./Scanner c_front.s
+
+rm -f libc_front.so
+g++ -std=c++11 -fPIC -shared CodeGenerate.cpp -L. -lScanner -o libc_front.so -Wl,-rpath .
