5697466032e2b3ae566c4df4ab5aa635d701fc7d
ProgramerArt123 <2039231428@qq.com>
2022-01-03 14:53:05 +0800

    variable

--- c_front.s
+++ c_front.s
@@ -15,6 +15,7 @@ expression:@ignore@@expression@@ignore@"--"
 expression:@ignore@@expression@@ignore@@oper_two@@ignore@@expression@@ignore@
 expression:@ignore@@expression@@ignore@"?"@ignore@@expression@@ignore@":"@ignore@@expression@@ignore@
 expression:(sizeof@round@)|(sizeof +@string@|@label@|@numeric@|@integer@)
+expression:@variable@|@numeric@|@integer@
 expression:"("@expression@")"
 
 oper_two:@oper_comma@|@oper_assgin@|@oper_logic_or@|@oper_logic_and@|@oper_bit_or@|@oper_bit_xor@|@oper_bit_and@|@oper_equal@|@oper_rela@|@oper_mov@|@oper_add_sub@|@oper_mul_div@
@@ -38,11 +39,13 @@ type_cast:"("@label@")"
 oper_suffix:"."|"->"
 round:"("@expression@")"
 
-keywords:auto|double|int|struct|break|else|long|switch|case|enum|register|typedef|char|extern|return|union|const|float|short|unsigned|continue|for|signed|void|default|goto|sizeof|volatile|do|if|while|static
-
 label:[_a-zA-Z][a-zA-Z0-9]*
+
+keywords:auto|double|int|struct|break|else|long|switch|case|enum|register|typedef|char|extern|return|union|const|float|short|unsigned|continue|for|signed|void|default|goto|sizeof|volatile|do|if|while|static
 
 numeric:@integer@\.@integer@
 
 integer:[0-9]+
+
+variable:@label@|@label@"["@expression@"]"
 
