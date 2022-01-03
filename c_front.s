8dc33f90ea4e2ed8e9986da0d0b2d73950dc7e6a
ProgramerArt123 <2039231428@qq.com>
2022-01-03 21:39:27 +0800

    reduce

--- c_front.s
+++ c_front.s
@@ -16,7 +16,7 @@ expression:@ignore@@expression@@oper_two@@expression@@ignore@
 expression:@ignore@@expression@"?"@expression@":"@expression@@ignore@
 expression:(@ignore@sizeof@ignore@@round@@ignore@)|(@ignore@sizeof(space|line)+@string@|@label@|@numeric@|@integer@@ignore@)
 expression:@ignore@@variable@|@numeric@|@integer@@ignore@
-expression:@ignore@"("@expression@")"@ignore@
+expression:@ignore@@round@@ignore@
 
 oper_two:@oper_comma@|@oper_assgin@|@oper_logic_or@|@oper_logic_and@|@oper_bit_or@|@oper_bit_xor@|@oper_bit_and@|@oper_equal@|@oper_rela@|@oper_mov@|@oper_add_sub@|@oper_mul_div@
 
@@ -41,7 +41,7 @@ round:"("@expression@")"
 
 label:[_a-zA-Z][a-zA-Z0-9]*
 
-keywords:struct|break|else|switch|case|enum|register|typedef|extern|return|union|const|unsigned|continue|for|signed|void|default|goto|sizeof|volatile|do|if|while|static
+keywords:struct|break|enum|register|typedef|extern|return|union|unsigned|continue|signed|void|goto|volatile
 
 numeric:@integer@\.@integer@
 
@@ -49,7 +49,7 @@ integer:[0-9]+
 
 variable:@pointer@*@label@("["@expression@"]")*
 
-base_type:auto|double|int|long|char|float
+base_type:auto|double|int|long|char|float|void
 
 type:@base_type@|@label@
 
@@ -67,34 +67,38 @@ pointer:"*"@ignore@
 
 address:"&"@ignore@@label@|"&"@ignore@@label@"["@expression@"]"
 
-scope:("{"@ignore@@statements@@ignore@"}")|(@ignore@@statement@@ignore@)
+block:"{"@ignore@@statements@@ignore@"}"
 
-condition:"("@ignore@@expression@@ignore@")"
+scope:@block@|(@ignore@@statement@@ignore@)
 
-if:if@ignore@@condition@@ignore@@scope@
+if:if@ignore@@round@@ignore@@scope@
 
-else_if:else@ignore@if@ignore@@condition@@ignore@@scope@
+else_if:else@ignore@if@ignore@@round@@ignore@@scope@
 
-else:else@ignore@@condition@@ignore@@scope@
+else:else@ignore@@round@@ignore@@scope@
 
 if_else:@if@(@ignore@@else_if@@ignore@)*(@ignore@@else@)?
 
-case:case@ignore@@variable@|@integer@@ignore@("{"@ignore@@statements@@ignore@"}")|(@ignore@@processes@@ignore@)
+branch:@variable@|@integer@@ignore@@block@|(@ignore@@processes@@ignore@)
 
-default:default@ignore@@variable@|@integer@@ignore@("{"@ignore@@statements@@ignore@"}")|(@ignore@@processes@@ignore@)
+case:case@ignore@@branch@
 
-switch_case:@ignore@"("@ignore@@expression@@ignore@")"@ignore@"{"@ignore@@case@*@default@?@ignore@"}"
+default:default@ignore@@branch@
 
-while:while@ignore@@condition@@ignore@@scope@
+switch_case:@ignore@@round@@ignore@"{"@ignore@@case@*@default@?@ignore@"}"
 
-do_while:do@ignore@@scope@@ignore@while@ignore@@condition@
+while:while@ignore@@round@@ignore@@scope@
 
-for:for@ignore@"("@ignore@@expression@@ignore@";"@ignore@@expression@@ignore@";"@ignore@@expression@@ignore@")"@ignore@"{"@ignore@@statements@@ignore@"}"
+do_while:do@ignore@@scope@@ignore@while@ignore@@round@
 
-arguments:@expression@(@ignore@@oper_comma@@ignore@@expression@)*
+comma_process:@processes@(@oper_comma@@processes@)*"
+
+for:for@ignore@"("@ignore@(@statement@@ignore@(@ignore@@oper_comma@@ignore@@statement@)*)|";"@ignore@@comma_process@@ignore@|";"@ignore@@comma_process@@ignore@|")"@ignore@@scope@
+
+arguments:@expression@(@ignore@@oper_comma@@expression@)*
 
 proc_call:@variable@"("arguments?")"
 
 parameters:	@arbitrarily_type@@label@(@ignore@@arbitrarily_type@@ignore@@label@)*
 
-proc_def:void|@arbitrarily_type@@ignore@"("@ignore@@parameters@?@ignore@")"@ignore@"{"@ignore@@statements@@ignore@"}"
+proc_def:@arbitrarily_type@@ignore@"("@ignore@@parameters@?@ignore@")"@ignore@@block@
