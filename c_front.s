42eefb590eff848e57fcc5b54a39c9ae1f15c843
ProgramerArt123 <2039231428@qq.com>
2022-01-08 15:50:08 +0800

    modify or

--- c_front.s
+++ c_front.s
@@ -7,44 +7,44 @@ space:[ \t\r]
 
 line:\n
 
-partition:space|line
+partition:[@space@@line@]
 
 ignore:@partition@*
 
 segmentation:@partition@+
 
-process:@expression@|@return@|@goto@|@proc_call@
+process:[@expression@@return@@goto@@proc_call@]
 
 variable_def:@arbitrarily_type@@ignore@@variable@@expression@?
 
-statement:@process@|@variable_def@
+statement:[@process@@variable_def@]
 
-expression:@ignore@@oper_one@@expression@@ignore@|@ignore@@expression@"++"@ignore@|@ignore@@expression@"--"@ignore@|@ignore@@expression@@oper_two@@expression@@ignore@|@ignore@@expression@"?"@expression@":"@expression@@ignore@|@ignore@@element@@ignore@|@ignore@@variable@|@numeric@|@integer@@ignore@|@ignore@@round@@ignore@
+expression:[@ignore@@oper_one@@expression@@ignore@)(@ignore@@expression@"++"@ignore@)(@ignore@@expression@"--"@ignore@)(@ignore@@expression@@oper_two@@expression@@ignore@)(@ignore@@expression@"?"@expression@":"@expression@@ignore@)(@ignore@@element@@ignore@)(@ignore@@variable@)(@numeric@)(@integer@@ignore@)(@ignore@@round@@ignore@)]
 
 
-element:@string@|@variable@|@numeric@|@integer@|@address@|@sizeof@
+element:[@string@@variable@@numeric@@integer@@address@@sizeof@]
 
-sizeof:(sizeof@ignore@"("@ignore@@expression@@ignore@")")|(sizeof@ignore@@element@)
+sizeof:[(sizeof@ignore@"("@ignore@@expression@@ignore@")")(sizeof@ignore@@element@)]
 
-oper_two:@oper_comma@|@oper_assgin@|@oper_logic_or@|@oper_logic_and@|@oper_bit_or@|@oper_bit_xor@|@oper_bit_and@|@oper_equal@|@oper_rela@|@oper_mov@|@oper_add_sub@|@oper_mul_div@
+oper_two:[@oper_comma@@oper_assgin@@oper_logic_or@@oper_logic_and@@oper_bit_or@@oper_bit_xor@@oper_bit_and@@oper_equal@@oper_rela@@oper_mov@@oper_add_sub@@oper_mul_div@]
 
 oper_comma:","	#printf(",\n")#
-oper_assgin:"="|"+="|"-="|"*="|"/="|"%="|"&="|"^="|"|="|"<<="|">>="
+oper_assgin:["=""+=""-=""*=""/=""%=""&=""^=""|=""<<="">>="]
 oper_logic_or:"||"
 oper_logic_and:"&&"
 oper_bit_or:"|"
 oper_bit_xor:"^"
 oper_bit_and:"&"
-oper_equal:"=="|"!="
-oper_rela:"<"|"<="|">"|">="
-oper_mov:"<<"|">>"
-oper_add_sub:"+"|"-"
-oper_mul_div:"*"|"/"|"%"
+oper_equal:["==""!="]
+oper_rela:["<""<="">"">="]
+oper_mov:["<<"">>"]
+oper_add_sub:["+""-"]
+oper_mul_div:["*""/""%"]
 
-oper_one:"-"|"++"|"--"|"!"|"~"|"+"|"-"|"*"|"&"
+oper_one:["-""++""--""!""~""+""-""*""&"]
 type_cast:"("@label@")"
 
-oper_suffix:"."|"->"
+oper_suffix:[".""->"]
 round:"("@expression@")"
 
 label:[_a-zA-Z][a-zA-Z0-9]*
@@ -55,33 +55,33 @@ integer:[0-9]+
 
 variable:@pointer@*@label@("["@expression@"]")*(@oper_suffix@@variable@)*
 
-base_type:(auto)|(double)|(int)|(((unsigned)|(signed))(@ignore@int|(long)|(char))?)|(long)|(char)|(float)|(void)
+base_type:["auto""double""int"["unsigned""signed"](@ignore["int""long""char"])?"long""char""float""void"]
 
-type:@base_type@|@label@
+type:[@base_type@@label@]
 
-const_type:(const@ignore@@type@)|(@type@@ignore@const)
+const_type:[("const"@ignore@@type@)(@type@@ignore@"const")]
 
-extern_static:(extern)|(static)
+extern_static:["extern""static"]
 
-extern_static_type:(@extern_static@@ignore@@type@)|(@type@@ignore@@extern_static@)
+extern_static_type:[(@extern_static@@ignore@@type@)|(@type@@ignore@@extern_static@)]
 
-register_volatile:(register)|(volatile)
+register_volatile:["register""volatile"]
 
-register_volatile_type:(@register_volatile@@ignore@@type@)|(@type@@ignore@@register_volatile@)
+register_volatile_type:[(@register_volatile@@ignore@@type@)(@type@@ignore@@register_volatile@)]
 
-extern_static_const:(@extern_static@@ignore@const)|(const@ignore@@extern_static@)
+extern_static_const:[(@extern_static@@ignore@const)(const@ignore@@extern_static@)]
 
-extern_static_const_type:(@extern_static_const@@ignore@@type@)|(@type@@ignore@@extern_static_const@)
+extern_static_const_type:[(@extern_static_const@@ignore@@type@)(@type@@ignore@@extern_static_const@)]
 
-extern_static_register_volatile:(@extern_static@@ignore@@register_volatile@)|(@register_volatile@@ignore@@extern_static@)
+extern_static_register_volatile:[(@extern_static@@ignore@@register_volatile@)(@register_volatile@@ignore@@extern_static@)]
 
-extern_static_register_volatile_type:(@extern_static_register_volatile@@ignore@@type@)|(@type@@ignore@@extern_static_register_volatile@)
+extern_static_register_volatile_type:[(@extern_static_register_volatile@@ignore@@type@)(@type@@ignore@@extern_static_register_volatile@)]
 
-const_register_volatile:(const@ignore@@register_volatile@)|(@register_volatile@@ignore@const)
+const_register_volatile:[(const@ignore@@register_volatile@)(@register_volatile@@ignore@const)]
 
-const_register_volatile_type:(@const_register_volatile@@ignore@@type@)|(@type@@ignore@@const_register_volatile@)
+const_register_volatile_type:[(@const_register_volatile@@ignore@@type@)(@type@@ignore@@const_register_volatile@)]
 
-arbitrarily_type:@const_register_volatile_type@|@extern_static_register_volatile_type@|@extern_static_const_type@|@register_volatile_type@|@extern_static_type@|@const_type@
+arbitrarily_type:[@const_register_volatile_type@@extern_static_register_volatile_type@@extern_static_const_type@@register_volatile_type@@extern_static_type@@const_type@]
 
 pointer:"*"@ignore@
 
@@ -89,7 +89,7 @@ address:"&"@ignore@@variable@
 
 block:"{"@ignore@@statement@*@ignore@"}"
 
-scope:@block@|(@ignore@@statement@@ignore@)
+scope:[@block@(@ignore@@statement@@ignore@)]
 
 if:if@ignore@@round@@ignore@@scope@
 
@@ -99,21 +99,21 @@ else:else@ignore@@round@@ignore@@scope@
 
 if_else:@if@(@ignore@@else_if@@ignore@)*(@ignore@@else@)?
 
-break_process:break|@process@
+break_process:["break"@process@]
 
-branch:@variable@|@integer@@ignore@":"@ignore@@block@|(@ignore@@break_process@+@ignore@)|
+branch:@integer@@ignore@":"@ignore@[@block@(@ignore@@break_process@+@ignore@)""]
 
-case:case@ignore@@branch@
+case:"case"@ignore@@branch@
 
 default:default@ignore@@branch@
 
 switch_case:@ignore@@round@@ignore@"{"@ignore@@case@*@default@?@ignore@"}"
 
-break_continue_statement:break|continue|@statement@
+break_continue_statement:["break""continue"@statement@]
 
 break_continue_block:"{"@ignore@@break_continue_statement@*@ignore@"}"
 
-once:@break_continue_block@|(@ignore@@break_continue_statement@@ignore@)
+once:[@break_continue_block@(@ignore@@break_continue_statement@@ignore@)]
 
 while:while@ignore@@round@@ignore@@once@
 
